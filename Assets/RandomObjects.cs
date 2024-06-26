25af099f0e274dd1f9ac2c7285e132b7c0753960
nacho3128 <ignaciosdelavega@gmail.com>
2024-06-26 16:53:02 -0300

    Uploaded RandomObjects script
    Solo aparecen 2 comidas aleatorias

--- Assets/RandomObjects.cs
+++ Assets/RandomObjects.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 public class RandomObjects : MonoBehaviour
 {
-    public GameObject[] Food; 
-
+    public GameObject[] Food;
+    int RandomIndex1;
+    int RandomIndex2;
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.SetActive(true); 
-      Random.Range(0, Food.Length);
+        foreach(GameObject fod in Food)
+        {
+            fod.SetActive(false);
+        }
+        RandomIndex1 = Random.Range(0, Food.Length);
+        RandomIndex2 = Random.Range(0, Food.Length);
+        if(RandomIndex2 == RandomIndex1)
+        {
+            RandomIndex2 = Random.Range(0, Food.Length);
+        }
+        Food[RandomIndex1].SetActive(true);
+        Food[RandomIndex2].SetActive(true);
     }
 
     // Update is called once per frame
